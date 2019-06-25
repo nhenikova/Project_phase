5ff733d266275089ffba852011c474f926066625
Naty <n.henikova@gmail.com>
2019-06-25 22:42:08 +0200

    creating new components header, cities, forecast, modifying the weather app, saving new assets images

--- angular-weather-app-master/src/app/app-routing.module.ts
+++ angular-weather-app-master/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CitiesComponent } from './cities/cities.component';
+import { ForecastComponent } from './forecast/forecast.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: CitiesComponent},
+  {path: 'forecast', component: ForecastComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- angular-weather-app-master/src/app/app.module.ts
+++ angular-weather-app-master/src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { CitiesComponent } from './cities/cities.component';
+import { ForecastComponent } from './forecast/forecast.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CitiesComponent,
+    ForecastComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
