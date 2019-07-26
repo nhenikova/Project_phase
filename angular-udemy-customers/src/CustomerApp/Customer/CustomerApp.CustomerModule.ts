7a6bc7d4b898727154e0734f9408f4208e19e399
Naty <n.henikova@gmail.com>
2019-07-27 00:01:14 +0200

    adding angular udemy customers, the whole project and its changes

---
+++ angular-udemy-customers/src/CustomerApp/Customer/CustomerApp.CustomerModule.ts
@@ -0,0 +1,35 @@
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import {FormsModule , ReactiveFormsModule} from "@angular/forms"
+import {RouterModule} from "@angular/router"
+import { CustomerComponent } from './CustomerApp.CustomerComponent';
+import {GridComponent} from "../Utility/CustomerApp.GridComponent"
+import { CustomerRoutes } from '../Routing/CustomerApp.CustomerRouting';
+import {HttpClientModule,HTTP_INTERCEPTORS} from "@angular/common/http"
+import { MyInterceptor } from '../Utility/Utility.HttpInterceptor';
+
+@NgModule({
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  declarations: [
+     CustomerComponent , GridComponent
+  ],
+  imports: [
+    RouterModule.forChild(CustomerRoutes),
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
+  ],
+  exports: [
+    GridComponent
+  ],
+  providers: [
+   {provide: HTTP_INTERCEPTORS, useClass: MyInterceptor , 
+  multi:true}
+
+  ],
+  bootstrap: [CustomerComponent]
+})
+export class CustomerModule { }
